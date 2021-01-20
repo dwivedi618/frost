36ece7d763415e7c1fe6e09e29c639e84418ee22
shivam dwivedi <shivamdwivedi618@gmail.com>
2021-01-20 17:34:19 +0530

    primary links (needs m)

--- src/app/layouts/primary-layout/primary-layout.component.ts
+++ src/app/layouts/primary-layout/primary-layout.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-primary-layout',
@@ -19,13 +17,11 @@ export class PrimaryLayoutComponent implements OnInit{
     { path : 'groups',icon :'groups', name : 'Groups' },
     { path : 'calls',icon :'call', name : 'Calls' },
     { path : 'files',icon :'folder', name : 'Files' },
-   
-
-  ];
+  ]  
 
   constructor() {}
   ngOnInit(){
 
- }
+  }
 
 }
