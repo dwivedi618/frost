02699edfb04efb6522fa0f1c130c0d8581c78d2d
shivam dwivedi <shivamdwivedi618@gmail.com>
2021-01-20 17:05:02 +0530

    primary Links

--- src/app/layouts/primary-layout/primary-layout.component.ts
+++ src/app/layouts/primary-layout/primary-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -8,14 +8,20 @@ import { map, shareReplay } from 'rxjs/operators';
   templateUrl: './primary-layout.component.html',
   styleUrls: ['./primary-layout.component.scss']
 })
-export class PrimaryLayoutComponent {
+export class PrimaryLayoutComponent implements OnInit{
+  primaryLinks = [
+    { path : '',icon :'notifications', name : 'Activity' },
+    { path : 'chats',icon :'message', name : 'Chats' },
+    { path : 'groups',icon :'groups', name : 'Groups' },
+    { path : 'calls',icon :'call', name : 'Calls' },
+    { path : 'files',icon :'folder', name : 'Files' },
+   
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  ];
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor() {}
+  ngOnInit(){
+
+ }
 
 }
