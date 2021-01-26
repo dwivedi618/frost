073696d58f6a91c3b573588842d2bc33f91727b4
shivam dwivedi <shivamdwivedi618@gmail.com>
2021-01-26 14:03:18 +0530

    append message to chats ,auto scroll to bottom when chat appends

--- src/app/modules/chat/chat-box/chat-box.component.ts
+++ src/app/modules/chat/chat-box/chat-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,27 +6,89 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: './chat-box.component.html',
   styleUrls: ['./chat-box.component.scss']
 })
-export class ChatBoxComponent implements OnInit {
-
-  messageForm : FormGroup;
+export class ChatBoxComponent implements OnInit, AfterViewChecked {
+  @ViewChild('scrollMe') private myScrollContainer: ElementRef;
+  messageForm: FormGroup;
+  chats = [
+    {
+      user_id: 1,
+      message: 'hi',
+      message_time: new Date()
+    },
+    {
+      user_id: 0,
+      message: 'helloo',
+      message_time: new Date()
+    },
+    {
+      user_id: 0,
+      message: 'how are you?',
+      message_time: new Date()
+    },
+    {
+      user_id: 0,
+      message: 'where have you been?',
+      message_time: new Date()
+    },
+    {
+      user_id: 1,
+      message: 'I have been busy.',
+      message_time: new Date()
+    },
+    {
+      user_id: 1,
+      message: 'umm.. hey! I hate to ask this but do you know where was marven last week?',
+      message_time: new Date()
+    },
+    {
+      user_id: 0,
+      message: 'what is this about?',
+      message_time: new Date()
+    },
+    {
+      user_id: 0,
+      message: 'is everything okay?',
+      message_time: new Date()
+    },
+  ]
   constructor(
-    public fb : FormBuilder
-  ) { 
+    public fb: FormBuilder
+  ) {
     this.messageForm = this.fb.group({
-      message : ['']
+      message: ['']
     })
   }
-
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
   ngOnInit(): void {
+    this.scrollToBottom();
+  }
+  scrollToBottom(): void {
+    try {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch (err) { }
   }
   toggled: boolean = false;
   handleSelection(event) {
     console.log(event.char);
-    const msg = this.messageForm.get('message') ;
-    msg.patchValue(msg.value +' '+ event.char)
+    const msg = this.messageForm.get('message');
+    msg.patchValue(msg.value + ' ' + event.char)
   }
 
-  send(){
+  send() {
     console.log(this.messageForm.value.message);
+    const msg = this.messageForm.get('message');
+    if (msg) {
+      const chat = {
+        user_id: 0,
+        message: this.messageForm.value.message,
+        message_time: new Date()
+      }
+      this.chats.push(chat);
+      this.messageForm.reset();
+      this.messageForm.value.message = ''
+    }
+
   }
 }
